8bf52a0c5a769bd5e74265faca9d2359e00277dc
karolina-nadDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD <westlinka20@gmail.com>
2021-08-30 12:26:27 +0300

    Updates

---
+++ precache-manifest.5feb88d998bb4694d93cb911180c723f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "300f07f91109dfc08b91fb139040d472",
+    "url": "./index.html"
+  },
+  {
+    "revision": "847ec4c728e72c13e683",
+    "url": "./static/css/main.ae4f840b.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "847ec4c728e72c13e683",
+    "url": "./static/js/main.9a3ceb7b.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
